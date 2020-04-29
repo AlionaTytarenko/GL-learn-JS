5d12c12b20f4cd8ff2bfdbbaac837dd78fd1d11f
otytaren <olena.tytarenko@harmonicinc.com>
2020-04-29 21:12:39 +0300

    check

---
+++ hw1.js
@@ -0,0 +1,10 @@
+//Task 1
+
+let name = "Генератор защитного поля";
+let price = 1000;
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
+let price = 2000;
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
